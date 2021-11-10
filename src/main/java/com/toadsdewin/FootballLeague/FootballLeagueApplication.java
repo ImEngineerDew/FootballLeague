efefc6642d5c43cfda17ed3b7ef066f7bbb211aa
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-11-09 20:56:41 -0500

    Adding the files for working the MongoDB

---
+++ src/main/java/com/toadsdewin/FootballLeague/FootballLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.toadsdewin.FootballLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toadsdewin/FootballLeague/FootballLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toadsdewin.FootballLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
