fca856a4902335e4099fb9b5e01b6ddab0d0e66a
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-11-22 21:26:11 -0500

    Making a new repository

---
+++ src/main/java/com/toadsdewin/FootballLeague/repositories/MatchRepository.java
@@ -0,0 +1,10 @@
+package com.toadsdewin.FootballLeague.repositories;
+import com.toadsdewin.FootballLeague.models.MatchModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface MatchRepository extends MongoRepository<MatchModel,String>
+{
+
+}
