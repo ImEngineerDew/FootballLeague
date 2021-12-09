f1738f2ef82933f003f6861de4bad09939bdbc2d
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-12-08 22:52:53 -0500

    I did it, I made the link between front and backend

--- src/main/java/com/toadsdewin/FootballLeague/controllers/TeamController.java
+++ src/main/java/com/toadsdewin/FootballLeague/controllers/TeamController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200/")  //Allows the connection with frontend
 @RequestMapping("/api")
 public class TeamController
 {
--- src/main/java/com/toadsdewin/FootballLeague/utils/Autorization.java
+++ src/main/java/com/toadsdewin/FootballLeague/utils/Autorization.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@Component
+//@Component
 public class Autorization implements Filter
 {
     public static final String KEY = "trolololo";
